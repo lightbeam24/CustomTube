8e3acc0b8620293b145b66f23ba0da9348af2340
lightbeam24 <105595945+lightbeam24@users.noreply.github.com>
2023-11-04 17:14:39 -0500

    1.2.4

--- chrome/scripts/resize.js
+++ chrome/scripts/resize.js
@@ -1,32 +1,30 @@
+document.addEventListener("yt-page-data-updated", delayedResize); 
 var loopThroughResize = setInterval(function() {
 	if (document.querySelector("html[location='watch'] ytd-watch-flexy:not([fullscreen]):not([theater]):not([cinema]) #movie_player") != null) {
-		var player = document.querySelector('#movie_player');
-		//var width = document.body.getAttribute('redux-player-width');
-		//var height = document.body.getAttribute('redux-player-height');
-		var offset = document.querySelector("html").getAttribute('player-offset');
-		var width = document.querySelector("html").getAttribute('player-width');
-		width = parseInt(width);
-		offset = parseInt(offset);
-		width = width + offset;
-		var height = document.querySelector("html").getAttribute('player-height');
-		player.setInternalSize(width, height);
+		doResize();
 	}
-}, 5000);
+}, 10000);
 setTimeout(loopThroughResize, 100);
 window.addEventListener('resize', () => {
-	let repeatInsert = setInterval(() => { //insert in loop for X seconds to prevent YT from overriding
-		var player = document.querySelector('#movie_player');
-		//var width = document.body.getAttribute('redux-player-width');
-		//var height = document.body.getAttribute('redux-player-height');
-		var offset = document.querySelector("html").getAttribute('player-offset');
-		var width = document.querySelector("html").getAttribute('player-width');
-		width = parseInt(width);
-		offset = parseInt(offset);
-		width = width + offset;
-		var height = document.querySelector("html").getAttribute('player-height');
-		player.setInternalSize(width, height);
+	let repeatInsert = setInterval(() => {
+		doResize();
 	}, 500);
 	setTimeout(() => {
 		clearInterval(repeatInsert);
 	}, 2000);
 });
+function delayedResize() {
+	console.log("delayedResize");
+	setTimeout(doResize, 1000);
+	setTimeout(doResize, 2500);
+}
+function doResize() {
+    var player = document.querySelector('#movie_player');
+	var offset = document.querySelector("html").getAttribute('player-offset');
+	var width = document.querySelector("html").getAttribute('player-width');
+	width = parseInt(width);
+	offset = parseInt(offset);
+	width = width + offset;
+	var height = document.querySelector("html").getAttribute('player-height');
+	player.setInternalSize(width, height);
+}

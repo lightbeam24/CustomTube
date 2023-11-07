e811fdaea40e74510119e70e182bdbdebfd243dd
lightbeam24 <105595945+lightbeam24@users.noreply.github.com>
2023-11-06 21:42:06 -0600

    1.2.4.2

--- chrome/scripts/resize.js
+++ chrome/scripts/resize.js
@@ -1,10 +1,24 @@
-document.addEventListener("yt-page-data-updated", delayedResize); 
-var loopThroughResize = setInterval(function() {
+//document.addEventListener("yt-page-data-updated", delayedResize); 
+/*var loopThroughResize = setInterval(function() {
 	if (document.querySelector("html[location='watch'] ytd-watch-flexy:not([fullscreen]):not([theater]):not([cinema]) #movie_player") != null) {
 		doResize();
 	}
-}, 10000);
-setTimeout(loopThroughResize, 100);
+}, 100000);*/
+var checkIfNeedResize = setInterval(function() {
+	if (document.querySelector("html[location='watch'] ytd-watch-flexy:not([fullscreen]):not([theater]):not([cinema]) #movie_player") != null) {
+		var ctrls = document.querySelector('.ytp-chrome-bottom');
+		var offset = document.querySelector("html").getAttribute('player-offset');
+		var width = document.querySelector("html").getAttribute('player-width');
+		width = parseInt(width);
+		offset = parseInt(offset);
+		width = width + offset;
+		var ctrlsWidth = ctrls.style.width;
+		if (ctrlsWidth != width) {
+			doResize();
+		}
+	}
+}, 2000);
+setTimeout(checkIfNeedResize, 10);
 window.addEventListener('resize', () => {
 	let repeatInsert = setInterval(() => {
 		doResize();
@@ -13,18 +27,22 @@ window.addEventListener('resize', () => {
 		clearInterval(repeatInsert);
 	}, 2000);
 });
+/*
 function delayedResize() {
 	console.log("delayedResize");
 	setTimeout(doResize, 1000);
 	setTimeout(doResize, 2500);
 }
+*/
 function doResize() {
-    var player = document.querySelector('#movie_player');
-	var offset = document.querySelector("html").getAttribute('player-offset');
-	var width = document.querySelector("html").getAttribute('player-width');
-	width = parseInt(width);
-	offset = parseInt(offset);
-	width = width + offset;
-	var height = document.querySelector("html").getAttribute('player-height');
-	player.setInternalSize(width, height);
+	if (document.querySelector("html[location='watch'] ytd-watch-flexy:not([fullscreen]):not([theater]):not([cinema]) #movie_player") != null) {
+		var player = document.querySelector('#movie_player');
+		var offset = document.querySelector("html").getAttribute('player-offset');
+		var width = document.querySelector("html").getAttribute('player-width');
+		width = parseInt(width);
+		offset = parseInt(offset);
+		width = width + offset;
+		var height = document.querySelector("html").getAttribute('player-height');
+		player.setInternalSize(width, height);
+	}
 }
